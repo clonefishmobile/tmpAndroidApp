66216993e2273c3a2d2ca3ef2889db2a408bf761
SerPyhisteg <m.sherbanich@yandex.ru>
2014-01-31 16:41:47 +0400

    Добавлен кликабельный текст.

--- FrepeeReader/src/com/example/frepeereader/MainActivity.java
+++ FrepeeReader/src/com/example/frepeereader/MainActivity.java
@@ -8,11 +8,17 @@ import java.io.InputStreamReader;
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -26,15 +32,29 @@ public class MainActivity extends Activity {
 	private static final String TRENDS  = "trends.txt";
 	private static final String MARTINGEIL  = "martingeil.txt";
 	
+	MainActivity activity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		activity = this;
 		setContentView(R.layout.main);
 		reader = (TextView) findViewById(R.id.readerView);
 		scroll = (ScrollView) findViewById(R.id.scrollView1);
+		makeContentList();
 		showText(COMMON);
 		showText(STRATEGY);
 		showText(TAKE_AND_RUN);
+		
+		SpannableString link = makeLinkSpan("scroll", new View.OnClickListener() {          
+	        @Override
+	        public void onClick(View v) {
+	           Toast.makeText(activity, "" + scroll.getScrollY(), Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    reader.append(link);
+	    reader.append("\n");
+		
 		showText(HAKIM_TWO);
 		showText(TRENDS);
 		showText(MARTINGEIL);
@@ -75,5 +95,62 @@ public class MainActivity extends Activity {
 			e1.printStackTrace();
 		}
 		reader.append("\n");
+		Log.d("reader","" + scroll.getMaxScrollAmount());
+	}
+	
+	private void makeContentList()
+	{
+		String[] content =  {
+			"Введение",
+			"Сорви 53$ и бери",
+			"Хаким-два",
+			"Последовательности и тренды",
+			"Усовершенствованная система мартингейла"};
+		
+		for (String item : content) {
+			makeContentItem(item);
+		}
+		 makeLinksFocusable(reader);
+		 reader.append("\n");
+	}
+	
+	private void makeContentItem(final String itemName)
+	{
+		SpannableString link = makeLinkSpan(itemName, new View.OnClickListener() {          
+	        @Override
+	        public void onClick(View v) {
+	           Toast.makeText(activity, itemName, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    reader.append(link);
+	    reader.append("\n");
+	   
+	}
+	
+	private SpannableString makeLinkSpan(CharSequence text, View.OnClickListener listener) {
+	    SpannableString link = new SpannableString(text);
+	    link.setSpan(new ClickableString(listener), 0, text.length(), 
+	        SpannableString.SPAN_INCLUSIVE_EXCLUSIVE);
+	    return link;
+	}
+	
+	private void makeLinksFocusable(TextView tv) {
+	    MovementMethod m = tv.getMovementMethod();  
+	    if ((m == null) || !(m instanceof LinkMovementMethod)) {  
+	        if (tv.getLinksClickable()) {  
+	            tv.setMovementMethod(LinkMovementMethod.getInstance());  
+	        }  
+	    }  
+	}
+	
+	private static class ClickableString extends ClickableSpan {  
+	    private View.OnClickListener mListener;          
+	    public ClickableString(View.OnClickListener listener) {              
+	        mListener = listener;  
+	    }          
+	    @Override  
+	    public void onClick(View v) {  
+	        mListener.onClick(v);  
+	    }        
 	}
 }
